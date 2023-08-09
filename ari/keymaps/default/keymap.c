213ff3da7eb11bbb120ac8c43f81f621e8fad69f
ari <agiterman@gmail.com>
2023-08-09 18:44:59 +0300

    minor fix

--- ari/keymaps/default/keymap.c
+++ ari/keymaps/default/keymap.c
@@ -59,10 +59,6 @@ combo_t key_combos[] = {
 #define K_PST    C(S(KC_V))
 #define K_CUT    C(S(KC_X))
 
-// home row mod
-#define ALT_S    ALT_T(KC_S)
-#define ALT_L    ALT_T(KC_L)
-
 #define LA_MOD MO(_MOD)
 // LA_MOD
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -70,11 +66,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,ALT_TAB ,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      CTL_ESC,    KC_A,   ALT_S,    KC_D,    KC_F,    KC_G,                     KC_H    ,KC_J    ,KC_K    ,ALT_L   ,KC_SCLN ,KC_QUOT ,
+      CTL_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_QUOT ,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      OSM_SFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                     KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,OSL_FUN ,
+      OS_SHFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                     KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,OSL_FUN ,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, KC_BSPC, SYM_LDR,   NAV_ENT ,KC_SPC  ,LA_MOD
+                                           OS_GUI, KC_BSPC, SYM_LDR,   NAV_ENT ,KC_SPC  ,LA_MOD
                                          // OSM_LCTL, GUI_ENT, SYM_TAB,   NAV_BSP ,KC_SPC  ,OSM_SFT
                                       //`--------------------------'  `--------------------------'
   ),
@@ -95,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       _______, KC_1,    KC_2,   KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______ ,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______,  OS_GUI,  OS_ALT, OS_SHFT, OS_CTRL,    KC_G,                     KC_H    ,OS_CTRL , OS_SHFT, OS_ALT ,  OS_GUI,KC_QUOT ,
+      _______,  OS_GUI,  OS_ALT, OS_SHFT, OS_CTRL, _______,                     _______ ,OS_CTRL , OS_SHFT, OS_ALT ,OS_GUI  ,_______ ,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       _______, _______, _______, _______, _______, _______,                     _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -105,11 +101,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_SYM] = LAYOUT(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      _______, KC_EXLM, KC_AT,  KC_HASH, KC_DLR,  KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______ ,
+      _______, KC_EXLM, KC_AT,  KC_HASH, KC_DLR,  KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN ,_______ ,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, KC_1,    KC_2,   KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______ ,
+      _______, KC_1,    KC_2,   KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0    ,_______ ,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, XXXXXXX, KC_TILD,KC_GRV, KC_LBRC, KC_LCBR,                       KC_RCBR, KC_RBRC, KC_COMM,KC_DOT,  KC_SLSH, _______ ,
+      _______, XXXXXXX, KC_TILD,KC_GRV, KC_LBRC, KC_LCBR,                       KC_RCBR, KC_RBRC, KC_COMM,KC_DOT,  KC_SLSH  ,_______ ,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_TRNS,  KC_TRNS, SYM   ,    KC_TRNS, KC_UNDS, KC_COLON
                                       //`--------------------------'  `--------------------------'
@@ -122,19 +118,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       _______, KC_HOME, KC_END , KC_MINS, KC_EQL , KC_PGDN,                      KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_APP ,_______ ,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, KC_LT  , KC_GT  , K_CPY  , K_PST  , KC_SCLN,                      KC_MPLY, KC_MPRV, KC_MNXT, KC_VOLD, KC_VOLU,_______ ,
+      _______, KC_LT  , KC_GT  , K_CPY  , K_PST  , KC_SCLN,                      KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT,KC_MPLY ,_______ ,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          CTL_ESC, KC_TRNS, XXXXXXX,    NAV  , KC_TRNS, KC_TRNS
+                                          KC_TRNS, KC_TRNS, XXXXXXX,    NAV  , KC_TRNS, KC_TRNS
                                       //`--------------------------'  `--------------------------'
   ),
 
   [_FUNC] = LAYOUT(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      _______, KC_F1  , KC_F2  , KC_F3   , KC_F4 ,  KC_F5 ,                     KC_F6   , KC_F7  , KC_F8  , KC_F9  , KC_F10 ,QK_LOCK ,
+      _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  ,  KC_F5 ,                     KC_F6   , KC_F7  , KC_F8  , KC_F9  , KC_F10 ,QK_LOCK ,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, KC_F11 , KC_F12 , XXXXXXX, XXXXXXX, XXXXXXX,                     KC_MS_L , KC_MS_D, KC_MS_U, KC_MS_R,XXXXXXX ,TO(_QWERTY),
+      _______, OS_GUI , OS_ALT , OS_SHFT, OS_CTRL,  KC_F11,                     KC_MS_L , KC_MS_D, KC_MS_U, KC_MS_R,XXXXXXX ,TO(_QWERTY),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, KC_CAPS, K_CUT  , K_CPY  , K_PST  , XXXXXXX,                     KC_WH_L , KC_WH_D, KC_WH_U, KC_WH_R,QK_BOOT ,_______ ,
+      _______, KC_CAPS, K_CUT  , K_CPY  , K_PST  ,  KC_F12,                     KC_WH_L , KC_WH_D, KC_WH_U, KC_WH_R,QK_BOOT ,_______ ,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                             ARI  , RANI   , XXXXXXX,    KC_BTN1, KC_BTN3, KC_BTN2
                                       //`--------------------------'  `--------------------------'
@@ -145,6 +141,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 long last_color;
 bool is_in_leader = false;
+
+// void keyboard_pre_init_user() {
+// }
 
 void keyboard_post_init_user() {
     register_led_msg();
@@ -176,17 +175,17 @@ bool is_oneshot_cancel_key(uint16_t keycode) {
 
 bool is_oneshot_ignored_key(uint16_t keycode) {
     switch (keycode) {
-     case LA_MOD:
+    case LA_MOD:
     // case LA_NAV:
     // case KC_LSFT:
     // case KC_BSPC:
     case CTL_ESC:
     case OSM_SFT:
-    case KC_LGUI:
-    case KC_BSPC:
     case SYM_LDR:
     case NAV_ENT:
-    case KC_SPC:
+    case OSL_FUN:
+    // case KC_SPC:
+    case KC_LGUI:
     case OS_SHFT:
     case OS_CTRL:
     case OS_ALT:
@@ -203,6 +202,7 @@ oneshot_state os_alt_state = os_up_unqueued;
 oneshot_state os_gui_state = os_up_unqueued;
 
 
+// uint8_t mod_state;
 uint16_t key_timer; // declare key_timer for use in macro
 bool is_alt_tab_active = false; // ADD this near the beginning of keymap.c
 uint16_t alt_tab_timer = 0;     // we will be using them soon.
@@ -229,7 +229,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     );
 
     // Store the current modifier state in the variable for later reference
-    static bool delkey_registered;
+    // static bool delkey_registered;
     uint8_t mod_state = get_mods();
     // If console is enabled, it will print the matrix position and status of each key pressed
 #ifdef CONSOLE_ENABLE
@@ -265,40 +265,41 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             break;
 
-        case GUI_BSP:
-                // of the delete key status: registered or not?
-            if (record->event.pressed) {
-                // Detect the activation of either shift keys
-#ifdef CONSOLE_ENABLE
-     uprintf("mod_state: 0x%04X, MOD_MASK_SHIFT: 0x%04X\n", mod_state, MOD_MASK_SHIFT);
-#endif
-                if (mod_state & MOD_MASK_SHIFT) {
-                    // First temporarily canceling both shifts so that
-                    // shift isn't applied to the KC_DEL keycode
-                    del_mods(MOD_MASK_SHIFT);
-                    register_code(KC_DEL);
-                    // Update the boolean variable to reflect the status of KC_DEL
-                    delkey_registered = true;
-                    // Reapplying modifier state so that the held shift key(s)
-                    // still work even after having tapped the Backspace/Delete key.
-                    set_mods(mod_state);
-                    return false; // Skip all further processing of this keyz
-                }
-            } else { // on release of KC_BSPC
-                // In case KC_DEL is still being sent even after the release of KC_BSPC
-                if (delkey_registered) {
-                    // uprintf("release delkey\n");
-                    unregister_code(KC_DEL);
-                    delkey_registered = false;
-                    return false; // Skip all further processing of this keyz
-                }
-            }
-            // Let QMK process the KC_BSPC keycode as usual outside of shift
-            return true;
+//         case GUI_BSP:
+//                 // of the delete key status: registered or not?
+//             if (record->event.pressed) {
+//                 // Detect the activation of either shift keys
+// // #ifdef CONSOLE_ENABLE
+//      uprintf("mod_state: 0x%04X, MOD_MASK_SHIFT: 0x%04X\n", mod_state, MOD_MASK_SHIFT);
+// // #endif
+//                 if (mod_state & MOD_MASK_SHIFT) {
+//                     // uprintf("shif hold\n");
+//                     // light_led( COLOR_LEADER );
+//                     // First temporarily canceling both shifts so that
+//                     // shift isn't applied to the KC_DEL keycode
+//                     del_mods(MOD_MASK_SHIFT);
+//                     register_code(KC_DEL);
+//                     // Update the boolean variable to reflect the status of KC_DEL
+//                     delkey_registered = true;
+//                     // Reapplying modifier state so that the held shift key(s)
+//                     // still work even after having tapped the Backspace/Delete key.
+//                     set_mods(mod_state);
+//                     return false; // Skip all further processing of this keyz
+//                 }
+//             } else { // on release of KC_BSPC
+//                 // In case KC_DEL is still being sent even after the release of KC_BSPC
+//                 if (delkey_registered) {
+//                     // uprintf("release delkey\n");
+//                     unregister_code(KC_DEL);
+//                     delkey_registered = false;
+//                     return false; // Skip all further processing of this keyz
+//                 }
+//             }
+//             // Let QMK process the KC_BSPC keycode as usual outside of shift
+//             return true;
 
         case ARI:
             if (record->event.pressed) {
-                // set_single_persistent_default_layer(_QWERTY);
                 // default_layer_set(_QWERTY);
                 set_single_persistent_default_layer(_QWERTY);
             }
@@ -306,7 +307,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
         case RANI:
             if (record->event.pressed) {
-                // set_single_persistent_default_layer(_RANI);
                 // default_layer_set(_RANI);
                 set_single_persistent_default_layer(_RANI);
                 light_led(COLOR_CONFIRM);
@@ -326,4 +326,6 @@ void matrix_scan_user(void) { // The very important timer.
     }
   }
 }
+
+
 
